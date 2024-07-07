243eae1e68dfc564541e55f594be8c355cdc7aad
Michael Torres <michael.46@live.it>
2024-07-07 15:54:27 +0200

    gsap importing

--- portfolio-mt/src/App.jsx
+++ portfolio-mt/src/App.jsx
@@ -1,4 +1,5 @@
-import "./index.css"; // O il percorso corretto al tuo file CSS
+import "./index.css";
+
 const App = () => {
   return <h1 className="bg-red-500 text-3xl text-center ">Michael Torres!</h1>;
 };
