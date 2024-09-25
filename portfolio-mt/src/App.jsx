1fc161ba13e53a37ac50f53b10293d281ee5ab04
Michael Torres <michael.46@live.it>
2024-09-25 05:35:37 +0200

    I'm Back

--- portfolio-mt/src/App.jsx
+++ portfolio-mt/src/App.jsx
@@ -1,7 +1,7 @@
 import "./index.css";
 
 const App = () => {
-  return <h1 className="bg-red-500 text-3xl text-center ">Michael Torres!</h1>;
+  return <h1 className="bg-red-500 text-4xl text-center ">Michael Torres!</h1>;
 };
 
 export default App;
